450200c53825fedae7e7aa62ed53c4e87a8e8962
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-05-02 18:09:38 -0500

    Initial commit

---
+++ src/test/java/com/lmlopez/vet_app/VetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmlopez.vet_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
