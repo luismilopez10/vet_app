e489ceb321f05075ee0bbddc6032060dbf1d1e09
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-05-02 18:32:33 -0500

    Entity package added

---
+++ src/main/java/com/lmlopez/vet_app/entity/Client.java
@@ -0,0 +1,34 @@
+package com.lmlopez.vet_app.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "Client")
+@Table(name = "client")
+@Data
+public class Client {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String cellphone;
+
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    private List<Pet> pets = new ArrayList<>();
+
+    public Client addPet(Pet pet){
+        this.pets.add(pet);
+
+        return this;
+    }
+}
---
+++ src/main/java/com/lmlopez/vet_app/entity/Pet.java
@@ -0,0 +1,24 @@
+package com.lmlopez.vet_app.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity(name = "Pet")
+@Table(name = "pet")
+@Data
+public class Pet {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String type;
+
+    private Character sex;
+
+    private Integer age;
+
+    private Long FK_client_id;
+}
